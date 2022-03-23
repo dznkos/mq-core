99f7b869a3b109d859f4485104378c453f2e20e0
Kse <kh@gmail.com>
2022-03-22 23:20:14 -0500

    Core Ver 0.5

---
+++ src/CoreApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export const CoreApp = () => {
+  return (
+    <div className='grid-container'>
+
+      <header className='header'>HEADER</header>
+      <nav className='navbar'>NAVBAR</nav>
+      <aside className='sidebar'>SIDEBAR</aside>
+      <article className='main'>MAIN</article>
+      <footer className='footer'>FOOTER</footer>
+
+    </div>
+  )
+}
